e4aed33be9296529a080aa3f46903c04b4dfcda3
joacomonsalvo <jmonsalvo.business@protonmail.com>
2021-10-08 15:37:29 -0300

    .

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
     extra_require={
         "dev": [
           "pytest >= 3.7",
+          "twine >= 3.4.0", 
         ],
     },
 
@@ -30,9 +31,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8"
-        "Programming Language :: Python :: 3.9"
-        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
